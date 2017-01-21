b5671d50210be4cf0a8739d95d7f53384a7a439d
seleb <sean.s.leblanc@gmail.com>
2017-01-21 14:13:29 -0500

    lap counting

--- client/player.js
+++ client/player.js
@@ -1,11 +1,14 @@
 
 
 DEFAULT_SPEED = 2;
+NUM_LAPS = 5;
 
 function Player( _id ){
     
     this.id = _id;
     this.lane = _id;
+    this.lapsRemaining = NUM_LAPS;
+
     this.container = new PIXI.Container();
     this.beanSprite = new PIXI.Sprite( PIXI.loader.resources.bean.texture );
     this.beanSprite.anchor.x = 0.5;
@@ -29,7 +32,7 @@ function Player( _id ){
 
 Player.prototype.update = function(){
 	var laneSize = poolBounds.height * 0.25;
-	
+
     this.lastX = this.container.x;
 
     var input = getInput( this.id );
@@ -46,13 +49,15 @@ Player.prototype.update = function(){
     
     this.container.x += this.speed * this.direction;
     
-    if( this.container.x + this.beanSprite.width/2 >= poolBounds.width + poolBounds.x ){
-        this.direction = -1;
+    if(
+    	( this.container.x + this.beanSprite.width/2 >= poolBounds.width + poolBounds.x )
+    	||
+        ( this.container.x - this.beanSprite.width/2 <= poolBounds.x )
+    ){
+        this.direction = -this.direction;
+        this.lapsRemaining -= 1;
     }
-    if( this.container.x - this.beanSprite.width/2 <= poolBounds.x ){
-        this.direction = 1;
-    }
-
+    
     this.container.y = poolBounds.y + laneSize * (this.lane + 0.5);
 }
 
